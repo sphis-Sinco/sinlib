dec4f322bd0efda5403dc60a1f21304f8b3ee17d
IdealistCat <sinconsistencia@gmail.com>
2025-02-09 07:57:03 -0600

    Testing stuff

--- test/source/PlayState.hx
+++ test/source/PlayState.hx
@@ -12,27 +12,37 @@ class PlayState extends FlxState
 {
 	override public function create()
 	{
+                // 0.0.1
+		TryCatch.tryCatch(() ->
+		{
+			trace('TryCatch: No Error');
+		});
+		TryCatch.tryCatch(() ->
+		{
+			throw 0;
+		}, {
+				errFunc: () ->
+				{
+					trace('TryCatch: Error');
+				}
+		});
+		trace('FileManager (v${FileManager.FILE_MANAGER_VERSION}): ${FileManager.readFile(FileManager.getDataFile('data-goes-here.txt'))}');
+		trace('Application version: ${Application.VERSION}');
+
+		// 0.0.2
+		trace('Cool array: ${Arrays.listFromString('1\n2\n3')}');
+		trace('SLG isDebug: ${SLGame.isDebug}');
+		trace('SLG isWatch: ${SLGame.isWatch}');
+		trace('Unit Testing Int (Winner): ');
+                UnitTesting.testIntReturn(() -> {unitTestFunc(0);}, 0);
+		trace('Unit Testing Int (Loser): ');
                 TryCatch.tryCatch(() -> {
-                        trace('TryCatch: No Error');
-                });
-                TryCatch.tryCatch(() -> {
-                        throw 0;
+                        UnitTesting.testIntReturn(() -> {unitTestFunc(1);}, 0);
                 }, {
                         errFunc: () -> {
-                                trace('TryCatch: Error');
+                                trace('It failed, obviously LMAO');
                         }
                 });
-                trace('FileManager (v${FileManager.FILE_MANAGER_VERSION}): ${FileManager.readFile(FileManager.getDataFile('data-goes-here.txt'))}');
-                trace('Application version: ${Application.VERSION}');
-
-                // 0.0.2
-                trace('Cool array: ${Arrays.listFromString('1\n2\n3')}');
-                trace('SLG isDebug: ${SLGame.isDebug}');
-                trace('SLG isWatch: ${SLGame.isWatch}');
-                trace('Unit Testing Int (Winner): ${UnitTesting.testIntReturn(() -> unitTestFunc(0), 0)}');
-                trace('Unit Testing Int (Loser): ${UnitTesting.testIntReturn(() -> unitTestFunc(1), 0)}');
-
-
 
 		super.create();
 	}
@@ -42,7 +52,8 @@ class PlayState extends FlxState
 		super.update(elapsed);
 	}
 
-        public function unitTestFunc(_return:Int = 0) {
-                return _return;
-        }
+	public function unitTestFunc(_return:Int = 0):Int
+	{
+		return _return;
+	}
 }
